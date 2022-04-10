a0f098889973dce2a286afa509a0aa6400418ba4
caijichitangjun <762893079@qq.com>
2022-04-10 17:01:41 +0800

    2022.4.10

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,16 +6,26 @@ module.exports = {
       'link', { rel: 'icon', href: '/assets/img/logo.jpg' }
     ]
   ],
+  theme: 'reco',
   themeConfig: {
     logo: '/assets/img/logo.jpg',
     nav: [
-      { text: '首页', link: '/', icon: '' },
+      { text: '首页', link: '/', icon: 'reco-home' },
       { text: '个人信息', link: '/personalInformation/my' },
-      { text: '基础学习', link: '/demo/vue' },
-      { text: '实例代码', link: '/code/property' },
-      { text: '帮助', link: '/help/h1' },
-      { text: '意见收集', link: '/opinionCollection' }
-    ]
+      { text: '实例代码', link: '/code/property', icon: 'reco-github' },
+      { text: '帮助', link: '/help/h1', icon: 'reco-faq' },
+      { text: '意见收集', link: '/opinionCollection', icon: 'reco-message' }
+    ],
+    blogConfig: {
+      category: {
+        location: 2,     // 在导航栏菜单中所占的位置，默认2
+        text: '基础学习' // 默认文案 “分类”
+      },
+      tag: {
+        location: 3,     // 在导航栏菜单中所占的位置，默认3
+        text: '标签',      // 默认文案 “标签”
+      },
+    }
   },
   plugins: {
     //以下为插件，需要add安装/添加
@@ -24,6 +34,5 @@ module.exports = {
       headerAnchorSelector: '.header-anchor'
     },
     '@vuepress/back-to-top': true,               //返回顶部小图标
-
   }
 }
---
+++ docs/demo/vue.md
@@ -0,0 +1,13 @@
+---
+title: 烤鸭的做法
+date: '2019-08-08 08:00:00'
+sidebar: 'auto'
+categories: 
+ - vue
+tags: 
+ - vue
+keys: 
+publish: true
+---
+
+123456
